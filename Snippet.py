63e4b2c349225b52345072f787f4793466b5b486
MeherHarshitha <51412124+MeherHarshitha@users.noreply.github.com>
2020-05-12 17:59:01 +0530

    Snippet.py
    Code snippets in Python to perform
    1.Assigning elements to different lists
    2.Accessing elements from a tuple
    3.Deleting different dictionary elements

---
+++ Snippet.py
@@ -0,0 +1,16 @@
+#assigning elements to different lists
+list1=[2,6,8]
+list1.append(1)
+list1.extend([3,9,0])
+list1[3]=7
+alpha=["python","panda","numpy"]
+alpha.append(10)
+
+#accessing elements from tuple
+tup=(4,5,20,18)
+print(tup)
+print(tup[1])
+
+#deleting different dictionary elements
+dict={1:"data science",2:"ML",3:"Pyhton",4:"AI",5:"DP"}
+del dict[3]
